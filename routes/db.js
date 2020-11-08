ae0c321d8b45f9362a598476ae0d7d1683ff2f4d
Admin <admin@Admins-MacBook-Pro.local>
2020-11-08 23:31:57 +0600

    trying model

--- routes/db.js
+++ routes/db.js
@@ -2,15 +2,35 @@ var express = require('express');
 var router = express.Router();
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect('mongodb://localhost:27017/tonobil', {useNewUrlParser: true, useUnifiedTopology: true});
 
-const Cat = mongoose.model('Cat', { 
-    name: String 
+const Car = mongoose.model('Advertise_Schema', { 
+    title: string,
+    basePrice: number,
+    onTheRoadPrice: number,
+    Type: string,
+    Brand:  string,
+    Model:  string,
+    Variant: string,
+    Year:  number,
+    Transmission:  string,
+    Engine: number,
+    Seat_capacity:  number,
+    MIleage:  number,
+    Color:  string,
+    Specification:  object,
+    Equipment:  object,
+    Seller:   object,
+    CreatedAt:  Date,
+    UpdatedAt:  Date,
+    OtherDetail:  string
+    // Specification
+
 });
 
-const kitty = new Cat({ name: 'Zildjian' });
+const toyota = new Car({ name: 'alion' });
 
-kitty.save().then(() => console.log('meow'));
+toyota.save().then(() => console.log('voom'));
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
