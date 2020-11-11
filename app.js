a4bffb5b34a1f4e102e22f8e7c4506b07ed6c6d7
Admin <admin@Admins-MacBook-Pro.local>
2020-11-12 05:42:45 +0600

    Add advrtice route

--- app.js
+++ app.js
@@ -14,5 +14,17 @@ con.on('open', ()=>{
   console.log("Mongo DB connected!");
 });
 
+// Routers
+const indexRoute = require('./routes/index');
+const advertiseRoute = require('./routes/advertise');
+
+// Use
+app.use('/',indexRoute);
+app.use('/advertise', advertiseRoute);
+
+app.listen(3000, ()=>{
+
+});
+
 // Export
 module.exports = app;
--- routes/advertise.js
+++ routes/advertise.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/',(req, res, next)=>{
-  console.log("Hello index!");
-  res.send("Hello index");
+    console.log("Advertise router");
+    res.send('Ads router');
 });
 
 module.exports = router;
