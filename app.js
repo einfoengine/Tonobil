e6038146e4452d99f0c427cc79408315ebf5bde2
Admin <admin@Admins-MacBook-Pro.local>
2020-11-12 03:54:54 +0600

    File organize, add mongoose, connect mongoos, On connection operation

--- app.js
+++ app.js
@@ -1,43 +1,18 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+// Express basic
+const express = require('express');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var dbRouter = require('./routes/db');
+// Mongo / Mongoose [Database]
+const mongoose = require('mongoose');
+const url = 'mongodb://localhost:27017/tonobil';
 
-var app = express();
+// App
+const app = express();
+mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true});
+const con = mongoose.connection;
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/db', dbRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+con.on('open', ()=>{
+  console.log("Mongo DB connected!");
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
+// Export
 module.exports = app;
