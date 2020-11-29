3217186f7796095b63740c5e39afa32e3225c4b8
Admin <admin@Admins-MacBook-Pro.local>
2020-11-29 06:22:30 +0600

    directoy add issue solve

--- index.js
+++ index.js
@@ -1,32 +1,29 @@
-// Express basic
 const express = require('express');
-
-// Hapi Joi
 const Joi = require('@hapi/joi');
-
-// Mongo / Mongoose [Database]
 const mongoose = require('mongoose');
-const url = 'mongodb://localhost:27017/tonobil';
+
+const config = require('./config')();
+
+const indexRoute = require('./lib/routes/index');
+const advertiseRoute = require('./lib/routes/advertise');
+const salersRoute = require('./lib/routes/salers');
+
 
 // App
 const app = express();
-mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(`mongodb://${config.mongo.host}:${config.mongo.port}/${config.db}`, {useNewUrlParser: true, useUnifiedTopology: true});
 const con = mongoose.connection;
 
 con.on('open', ()=>{
   console.log("Mongo DB connected!");
 });
 
-// Routers
-const indexRoute = require('./lib/routes/index');
-const advertiseRoute = require('./lib/routes/advertise');
-const salersRout = require('./lib/routes/salers');
 
 // Use
 app.use(express.json());
 app.use('/',indexRoute);
 app.use('/advertise', advertiseRoute);
-app.use('/salers', salersRout);
+app.use('/salers', salersRoute);
 
 app.listen(3000, ()=>{
 
