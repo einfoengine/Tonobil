380cfad619f15f749c04642eebb098fbad66ff09
Admin <admin@Admins-MacBook-Pro.local>
2020-12-03 13:49:17 +0600

    Client folder arrangement

--- index.js
+++ index.js
@@ -1,33 +1,16 @@
 const express = require('express');
-const Joi = require('@hapi/joi');
-const mongoose = require('mongoose');
-
-const config = require('./config')();
 
 const indexRoute = require('./lib/routes/index');
 const advertiseRoute = require('./lib/routes/advertise');
 const salersRoute = require('./lib/routes/salers');
+const {connectDb} = require('./lib/client/mongo');
+connectDb();
 
-
-// App
 const app = express();
-mongoose.connect(`mongodb://${config.mongo.host}:${config.mongo.port}/${config.db}`, {useNewUrlParser: true, useUnifiedTopology: true});
-const con = mongoose.connection;
 
-con.on('open', ()=>{
-  console.log("Mongo DB connected!");
-});
-
-
-// Use
 app.use(express.json());
-app.use('/',indexRoute);
+app.use('/', indexRoute);
 app.use('/advertise', advertiseRoute);
 app.use('/salers', salersRoute);
 
-app.listen(3000, ()=>{
-
-});
-
-// Export
 module.exports = app;
